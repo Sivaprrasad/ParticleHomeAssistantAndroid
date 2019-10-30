d33e33202e951d139676e0da27dd7c2ec160ff4c
Sivaprrasad Uppalapati <usivaprasad95@gmail.com>
2019-10-30 11:40:12 -0400

    - Added Button which takes the speech input using textview - Used in built android voice recognizer - Implemented the code from a Github resource from smartherd/SpeechtoText.

--- app/src/main/java/com/example/androidparticlestarter/MainActivity.java
+++ app/src/main/java/com/example/androidparticlestarter/MainActivity.java
@@ -1,22 +1,19 @@
 package com.example.androidparticlestarter;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
+import android.speech.RecognizerIntent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
-import com.google.cloud.speech.v1.SpeechClient;
-import com.google.cloud.speech.v1.RecognitionAudio;
-import com.google.cloud.speech.v1.RecognitionConfig;
-import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
-import com.google.cloud.speech.v1.RecognizeResponse;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import io.particle.android.sdk.cloud.ParticleCloud;
 import io.particle.android.sdk.cloud.ParticleCloudSDK;
@@ -24,19 +21,16 @@ import io.particle.android.sdk.cloud.ParticleDevice;
 import io.particle.android.sdk.cloud.exceptions.ParticleCloudException;
 import io.particle.android.sdk.utils.Async;
 
-public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+public class MainActivity extends AppCompatActivity {
+    private TextView Result;
 
-    Button buttonOn;
-    Button buttonOff;
-
-    private int RESULT;
-    TextToSpeech textToSpeech;
+    String comp ;
     // MARK: Debug info
     private final String TAG="SMARTLIGHT";
 
     // MARK: Particle Account Info
     private final String PARTICLE_USERNAME = "usivaprasad95@gmail.com";
-    private final String PARTICLE_PASSWORD = "Sivaprasad@95";
+    private final String PARTICLE_PASSWORD = "Sivaprasad@@95";
 
     // MARK: Particle device-specific info
     private final String DEVICE_ID = "300023001047363333343437";
@@ -51,6 +45,7 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Result = (TextView) findViewById(R.id.outputTV);
 
         // 1. Initialize your connection to the Particle API
         ParticleCloudSDK.init(this.getApplicationContext());
@@ -58,22 +53,21 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         // 2. Setup your device variable
         getDeviceFromCloud();
 
-        textToSpeech = new TextToSpeech(this, this);
-        buttonOn = findViewById(R.id.button);
-        buttonOff = findViewById(R.id.button2);
-
-        buttonOn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                RESULT = performClicks();
-                buttonOn.setText(String.valueOf(RESULT));
-                textToSpeech.speak(String.valueOf(RESULT), TextToSpeech.QUEUE_ADD, null);
-            }
-        });
-
-
     }
 
+    public void getVoiceInputHere(View view) {
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 1);
+        }
+        else {
+            Toast.makeText(this, "Does not support", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     /**
      * Custom function to connect to the Particle Cloud and get the device
@@ -88,7 +82,6 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
                 particleCloud.logIn(PARTICLE_USERNAME, PARTICLE_PASSWORD);
                 mDevice = particleCloud.getDevice(DEVICE_ID);
                 return -1;
-
             }
 
             @Override
@@ -104,103 +97,120 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         });
     }
 
-
-
-    public void turnLightsOnPressed(View view) {
-        Toast.makeText(getApplicationContext(), "On pressed", Toast.LENGTH_SHORT)
-                .show();
-
-        Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
-            @Override
-            public Object callApi(@NonNull ParticleCloud particleCloud) throws ParticleCloudException, IOException {
-                // put your logic here to talk to the particle
-                // --------------------------------------------
-
-                // what functions are "public" on the particle?
-                Log.d(TAG, "Availble functions: " + mDevice.getFunctions());
-
-
-                List<String> functionParameters = new ArrayList<String>();
-                //functionParameters.add();
-                try {
-                    mDevice.callFunction("turnLightsOn", functionParameters);
-
-                } catch (ParticleDevice.FunctionDoesNotExistException e1) {
-                    e1.printStackTrace();
-                }
-
-
-                return -1;
-            }
-
-            @Override
-            public void onSuccess(Object o) {
-                // put your success message here
-                Log.d(TAG, "Success!");
-            }
-
-            @Override
-            public void onFailure(ParticleCloudException exception) {
-                // put your error handling code here
-                Log.d(TAG, exception.getBestMessage());
-            }
-        });
-
-
-
-
-
-
-
-    }
-    public void turnLightsOffPressed(View view) {
-        Toast.makeText(getApplicationContext(), "Off pressed", Toast.LENGTH_SHORT)
-                .show();
-
-
-
-        Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
-            @Override
-            public Object callApi(@NonNull ParticleCloud particleCloud) throws ParticleCloudException, IOException {
-                // put your logic here to talk to the particle
-                // --------------------------------------------
-
-                // what functions are "public" on the particle?
-                Log.d(TAG, "Availble functions: " + mDevice.getFunctions());
-
-
-                List<String> functionParameters = new ArrayList<String>();
-                //functionParameters.add();
-                try {
-                    mDevice.callFunction("turnLightsOff", functionParameters);
-
-                } catch (ParticleDevice.FunctionDoesNotExistException e1) {
-                    e1.printStackTrace();
-                }
-
-
-                return -1;
-            }
-
-            @Override
-            public void onSuccess(Object o) {
-                // put your success message here
-                Log.d(TAG, "Success!");
-            }
-
-            @Override
-            public void onFailure(ParticleCloudException exception) {
-                // put your error handling code here
-                Log.d(TAG, exception.getBestMessage());
-            }
-        });
-
-
-
-    }
-
     @Override
-    public void onInit(int status) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
+            @Override
+            public Object callApi(@NonNull ParticleCloud particleCloud) throws ParticleCloudException, IOException {
+                // put your logic here to talk to the particle
+                // --------------------------------------------
+
+                // what functions are "public" on the particle?
+                Log.d(TAG, "Availble functions: " + mDevice.getFunctions());
+
+                switch (requestCode) {
+                    case 1:
+
+                        if (resultCode == RESULT_OK && data != null) {
+                            ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                            try{
+                                Result.setText(result.get(0));
+                            }
+
+                            catch (Exception e){
+                                Log.d(TAG, "Did not used API");
+                            }
+
+                            if ((Result.getText().toString().equals("on"))
+                                    || (Result.getText().toString().equals("turn lights on"))
+                                    || (Result.getText().toString().equals("lights on"))
+                                    || (Result.getText().toString().equals("turn leds on"))) {
+                                try{
+                                    mDevice.callFunction("lightsOn", result);
+                                }
+                                catch (ParticleDevice.FunctionDoesNotExistException e1) {
+                                    e1.printStackTrace();
+                                }
+                            }
+                            else  if ((Result.getText().toString().equals("off"))
+                                    || (Result.getText().toString().equals("turn lights off"))
+                                    || (Result.getText().toString().equals("lights off"))
+                                    || (Result.getText().toString().equals("turn leds off"))) {
+                                try {
+                                    mDevice.callFunction("lightsOff", result);
+                                }
+                                catch (ParticleDevice.FunctionDoesNotExistException e1) {
+                                    e1.printStackTrace();
+                                }
+                            }
+                        }
+                        break;
+                }
+                return -1;
+            }
+
+            @Override
+            public void onSuccess(Object o) {
+                // put your success message here
+                Log.d(TAG, "Success!");
+            }
+
+            @Override
+            public void onFailure(ParticleCloudException exception) {
+                // put your error handling code here
+                Log.d(TAG, exception.getBestMessage());
+            }
+        });
 
     }
+
+//    public void turnLightsOffPressed(View view) {
+//        Toast.makeText(getApplicationContext(), "Off pressed", Toast.LENGTH_SHORT)
+//                .show();
+//
+//
+//
+//        Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
+//            @Override
+//            public Object callApi(@NonNull ParticleCloud particleCloud) throws ParticleCloudException, IOException {
+//                // put your logic here to talk to the particle
+//                // --------------------------------------------
+//
+//                // what functions are "public" on the particle?
+//                Log.d(TAG, "Availble functions: " + mDevice.getFunctions());
+//
+//
+//                List<String> functionParameters = new ArrayList<String>();
+//                //functionParameters.add();
+//                try {
+//                    mDevice.callFunction("turnLightsOff", functionParameters);
+//
+//                } catch (ParticleDevice.FunctionDoesNotExistException e1) {
+//                    e1.printStackTrace();
+//                }
+//
+//
+//                return -1;
+//            }
+//
+//            @Override
+//            public void onSuccess(Object o) {
+//                // put your success message here
+//                Log.d(TAG, "Success!");
+//            }
+//
+//            @Override
+//            public void onFailure(ParticleCloudException exception) {
+//                // put your error handling code here
+//                Log.d(TAG, exception.getBestMessage());
+//            }
+//        });
+//
+//
+//
+//    }
 }
+
+
